73a39e433545e4a1f0ea2af55e02d7c42e07358b
vitadacoder <97452095+vitadacoder@users.noreply.github.com>
2022-01-11 19:09:34 +0100

    Variabili in  java

---
+++ Java/Cosa_sono_le_variabili_nella_programmazione_/Variabili.java
@@ -0,0 +1,30 @@
+package Java.Cosa_sono_le_variabili_nella_programmazione_;
+
+/**
+ * Variabili
+ */
+public class Variabili {
+
+    public static void main(String[] args) {
+        
+        /* Dichiarazione ed Inizializzazione */
+        int myInt; /* dichiarazione */
+        myInt = 10; /* inizializzazione */
+    
+        int number = 10; /* dichiarazione ed inizializzazione */
+    
+        /* In Java abbiamo 8 tipi di dato primitivi */
+        int myNum = 5;              // Integer
+        float myFloatNum = 5.99f;   // Floating point number
+        char myLetter = 'D';        // Character
+        boolean myBool = true;      // Boolean
+        String myText = "Hello";    // String
+        byte myByte = 100;          // Byte
+        short myShort = 5000;       // Short
+        long myLong = 1234567890L;  // Long
+
+    }
+
+}
+
+/** @vitadacoder */
